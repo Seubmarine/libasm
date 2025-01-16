b5cda173f93b5ffb72ad4eb92f13c555379fb631
Seubmarine <seubmarinepro@gmail.com>
2025-01-16 18:08:30 +0100

    fix strcmp
    valgrind didn't like the instruction cmpsb for some reason

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -4,19 +4,17 @@ SECTION .text
 GLOBAL ft_strcmp
 
 ft_strcmp:                  ; rdi = s1 ; rsi = s2
-    cld                     ; clear direction flag to go from left to right
-    L1:
-    cmp byte [rdi], 0 ; if *s1 == '\0'
-    je end
-    cmp byte [rsi], 0 ; if *s2 == '\0'
-    je end
-    cmpsb ; while (*s1 == *s2); s1++ s2++;
-    je L1 ; loop
-
-    dec rdi                 ; s1--;
-    dec rsi                 ; s2--;
-    end:
-    movzx rax, byte [rdi]   ; put *s1 into destination register for subsraction
-    movzx rsi, byte [rsi]   ; s2 becom *s2
-    sub rax, rsi             ; s1 - s2
-    ret
+        xor     r8, r8      ; u64 i = 0
+.loop:
+        movzx   eax, BYTE [rdi+r8] ; u8 c1 = s1[i]
+        movzx   ecx, BYTE [rsi+r8] ; u8 c2 = s2[i]
+        test    eax, eax                  ; c1 != '\0' (only need to test 1)
+        je      .end_of_string          ; 
+        inc     r8                     ; i++
+        cmp     al, cl                  ; c1 == c2
+        je      .loop
+.end_of_string:
+        movzx   eax, al                 ; cast char to a int, 0 extend the rest of the register
+        movzx   ecx, cl                 ; 
+        sub     eax, ecx                ; c1 - c2 and return directly
+        ret
